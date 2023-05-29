69b315567d4bf9d09c8763bddea2d367eb4b2faf
ThinhVu <vutrongthinhk7@gmail.com>
2023-05-29 12:15:46 +0700

    Update layout: Strip event from class

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,10 +1,20 @@
 <template>
   <div :class="attrsClasses">
-    <slot></slot>
+    <slot/>
   </div>
 </template>
 <script setup>
 import {computed, useAttrs} from 'vue'
+import {IGNORE_EVENT_MAP} from './Layout.helper'
 const attrs = useAttrs()
-const attrsClasses = computed(() => Object.keys(attrs));
+const attrsClasses = computed(() => {
+  const classes = []
+  const attrKeys = Object.keys(attrs)
+  for (const attrKey of attrKeys) {
+    if (IGNORE_EVENT_MAP[attrKey])
+      continue
+    classes.push(attrKey)
+  }
+  return classes
+})
 </script>
--- src/examples/LayoutExample.vue
+++ src/examples/LayoutExample.vue
@@ -1,34 +1,53 @@
 <template>
   <div>
     <h3>Layout example</h3>
+    <p>1. Layout with directive v-if</p>
     <layout fr ai-c fg-10px mb-2 v-if="show">
       Hide
     </layout>
 
+    <p>2. Layout with directive v-for</p>
     <layout v-for="i in [1,2]" :key="i">
-      {{i}}
+      Item {{i}}
     </layout>
 
-    <layout fr ai-c fg-10px mb-2>
-      <button>1</button>
-      <button>2</button>
+    <p>3. Layout with event handle</p>
+    <layout fr ai-c fg-10px mb-2 @click="clickMe">
+      <button @click="clickMe(1)">1</button>
+      <button @click="clickMe(2)">2</button>
       <icon>fas fa-times@20:red</icon>
     </layout>
 
+    <Abc @click="clickMe('abc')"></Abc>
+    <!-- region hide -->
+    <p>4. Layout flex column</p>
     <layout fc ai-c fg-10px mb-2>
       <button>1</button>
       <button>2</button>
       <icon>fas fa-times@20:red</icon>
     </layout>
 
+    <p>5. Layout grid</p>
     <layout grid gtc-1fr-2fr-50px ai-c fg-10px mb-2>
       <button>1</button>
       <button>2</button>
       <icon>fas fa-times@20:red</icon>
     </layout>
+    <!-- endregion -->
   </div>
 </template>
-<script setup>
+<script setup lang="jsx">
+const Abc = {
+  setup() {
+    return () => <layout>ABC</layout>
+  }
+}
+
 import {ref} from 'vue'
 const show = ref(false)
+const clickMe = (x) => console.log('click hit', x)
+
+const fn = () => console.log('hey there')
+
+
 </script>
