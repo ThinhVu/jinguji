2698e0fecf82dc62ed1d8e097fa02117dfadee82
ThinhVu <vutrongthinhk7@gmail.com>
2023-06-10 14:36:29 +0700

    update page header color

--- src/components/TPageHeader.vue
+++ src/components/TPageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fr ai-c jc-sb px-6px h-50px" style="background-color: rgba(0,0,0,0.2); color: #fff">
+  <div class="fr ai-c jc-sb px-6px h-50px" style="background-color: #24292f; color: #fff">
     <slot></slot>
     <div class="f1"></div>
     <h3>{{title}}</h3>
