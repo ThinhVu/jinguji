f03f8399c87a85d70e717e6494ee4e93af3fe467
ThinhVu <vutrongthinhk7@gmail.com>
2023-06-29 01:36:40 +0700

    update TTable.vue

--- src/components/TTable.vue
+++ src/components/TTable.vue
@@ -1,11 +1,18 @@
 <template>
-  <table><slot/></table>
+  <div class="t-table ovf-y-s sb-h">
+    <table><slot/></table>
+  </div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+.t-table {
+  border-radius: 6px;
+  border: thin solid #d0d7de;
+}
+
 table {
   border-collapse: separate;
   border-spacing: unset;
@@ -16,7 +23,7 @@ table {
 :deep(th) {
   padding: 0 8px;
   color: #1f2328;
-  border: thin solid #d0d7de;;
+  border: thin solid #d0d7de;
   text-align: left;
   background-color: #f6f8fa;
   position: sticky;
@@ -24,29 +31,13 @@ table {
   height: 48px;
 }
 
-:deep(th:first-child) {
-  border-radius: 6px 0 0 0;
-}
-
-:deep(th:last-child) {
-  border-radius: 0 6px 0 0;
-}
-
 :deep(td) {
   height: 36px;
   padding: 8px;
   color: #1f2328;
-  border: thin solid #d0d7de;;
+  border: thin solid #d0d7de;
   background-color: #fff;
   text-align: left;
-}
-
-:deep(tr:last-child > td:first-child) {
-  border-radius: 0 0 0 6px;
-}
-
-:deep(tr:last-child > td:last-child) {
-  border-radius: 0 0 6px 0;
 }
 
 :deep(tr:hover > td) {
