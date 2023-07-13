6b4f8f3302b0ce4ab18a99c43743ed34d2a7e77e
ThinhVu <vutrongthinhk7@gmail.com>
2023-07-13 23:51:13 +0700

    r 1.0.9, update TKeyValue

--- src/components/TKeyValue.vue
+++ src/components/TKeyValue.vue
@@ -2,14 +2,20 @@
   <div>
     <div class="fr ai-c fg-4px fs-s h-30px c:#1F2328">
       <span>{{label}}</span>
-      <t-btn v-if="!hasProps"
-          primary @click="addKV"
-             class="fr ai-c jc-c"
+      <t-btn v-if="hasProps"
+             @click="toggleExpand"
+             primary style="border-radius: 100px; width: 20px; height: 20px; padding: 0px">
+        <t-icon v-if="collapsed">fas fa-chevron-down@16px:#fff</t-icon>
+        <t-icon v-else>fas fa-chevron-up@16px:#fff</t-icon>
+      </t-btn>
+      <t-btn v-if="!hasProps || collapsed"
+             @click="addKV"
+             primary class="fr ai-c jc-c"
           style="border-radius: 100px; width: 20px; height: 20px; padding: 0px">
         <t-icon>fas fa-plus@16px:#fff</t-icon>
       </t-btn>
     </div>
-    <div v-if="hasProps" class="grid gtc-1fr-1fr-50px mt-1" style="grid-column-gap: 4px; grid-row-gap: 12px">
+    <div v-if="!collapsed && hasProps" class="grid gtc-1fr-1fr-50px mt-1" style="grid-column-gap: 4px; grid-row-gap: 12px">
       <template v-for="(v, k) in iModelValue" :key="k">
         <t-text :model-value="k" @update:modelValue="updateKey(k, $event)" :debounce-ms="500" placeholder="Key"/>
         <t-text :model-value="v" @update:modelValue="updateValue(k, $event)" :debounce-ms="500" placeholder="Value"/>
@@ -18,7 +24,7 @@
         </t-btn>
       </template>
     </div>
-    <t-btn v-if="hasProps"
+    <t-btn v-if="!collapsed && hasProps"
            primary @click="addKV"
            class="fr ai-c jc-c mt-1"
            style="border-radius: 100px; width: 20px; height: 20px; padding: 0px">
@@ -37,8 +43,13 @@ const emit = defineEmits(['update:modelValue'])
 const iModelValue = ref(props.modelValue || {})
 watch(() => props.modelValue, v => iModelValue.value = v || {})
 const hasProps = computed(() => Object.keys(props.modelValue).length)
+const collapsed = ref(true)
+function toggleExpand() {
+  collapsed.value = !collapsed.value
+}
 
 function addKV() {
+  collapsed.value = false
   iModelValue.value[''] = ''
   updateModelValue()
 }
